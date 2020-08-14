cb32b844fe8795e629ecfc02c3b76d2460af2754
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-08-14 19:30:00 +0900

    Add files via upload

---
+++ hashing_method.py
@@ -0,0 +1,42 @@
+# 체인법으로 해시 함수 구현하기
+
+from __future__ import annotations
+from typing import Any,Type
+import hashlib
+
+
+
+
+class Node:
+
+    def __init__(self,key:Any, value:Any,next:Node) ->None:
+        self.key = key
+        self.value = value
+        self.next = next
+
+class ChaineHash:
+    
+    def __init__(self, capacity: int) -> None:
+        self.capacity = capacity # 해시 테이블의 크기를 지정
+        self.table = [None]*self.capacity # 해시테이블(리스트) 선언
+
+    def hash_value(self,key:Any) -> int:
+        if isinstance(key, int):
+            return key % self.capacity
+        return(int(hashlib.sha256(str(key).encode)).hexdigest(),16)% self.capcity)
+
+    def search(self, key:Any) -> Any:
+        """키가 key인 원소를 검색하여 값을 반환"""
+
+        hash = self.hash_value(key)  # 검색하는 키의 해시값
+        p = self.table[hash]         # 노드를 주목
+
+        while is not None:
+            if p.key == key:
+                return p.value
+            p = p.next
+        
+        return None
+
+    
+
