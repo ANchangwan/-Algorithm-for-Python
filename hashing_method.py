806db1fc48f928cd371ed8e106016c7312a3c02c
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-08-17 22:03:32 +0900

    Add files via upload

--- hashing_method.py
+++ hashing_method.py
@@ -8,22 +8,25 @@ import hashlib
 
 
 class Node:
-
-    def __init__(self,key:Any, value:Any,next:Node) ->None:
-        self.key = key
-        self.value = value
-        self.next = next
+    """해시를 구성하는 노드"""
+    def __init__(self, key:Any, value:Any,next:Node) ->None:
+        """초기화"""
+        self.key = key      #키
+        self.value = value  #값
+        self.next = next    #뒤쪽 노드를 참조-
 
 class ChaineHash:
     
     def __init__(self, capacity: int) -> None:
-        self.capacity = capacity # 해시 테이블의 크기를 지정
-        self.table = [None]*self.capacity # 해시테이블(리스트) 선언
+        """초기화"""
+        self.capacity = capacity            # 해시 테이블의 크기를 지정
+        self.table = [None]*self.capacity   # 해시테이블(리스트) 선언
 
     def hash_value(self,key:Any) -> int:
-        if isinstance(key, int):
+        """해시값을 구함"""
+        if isinstance(key, int): # isinstance --> 특정 클래스/데이터 타입인지 검사해주느 함수, True or false를 리턴해준다.
             return key % self.capacity
-        return(int(hashlib.sha256(str(key).encode)).hexdigest(),16)% self.capcity)
+        return(int(hashlib.sha256(str(key).encode()).hexdigest(),16)% self.capacity)
 
     def search(self, key:Any) -> Any:
         """키가 key인 원소를 검색하여 값을 반환"""
@@ -31,12 +34,52 @@ class ChaineHash:
         hash = self.hash_value(key)  # 검색하는 키의 해시값
         p = self.table[hash]         # 노드를 주목
 
-        while is not None:
+        while p is not None:
             if p.key == key:
                 return p.value
             p = p.next
         
         return None
 
+    def add(self,key:Any,value:Any) -> bool:
+        """키가 key이고 값이 value인 원소를 추가"""
+        hash = self.hash_value(key)
+        p = self.table[hash]
+
+        while p is not None:
+            if p.key == key:
+                return False # 추가 실패
+            p = p.next        # 뒤쪽 노드를 주목
+
+        temp = Node(key, value, self.table[hash])
+        self.table[hash] = temp # 노드를 추가
+        return True             # 추가 성공
+
+    def remove(self, key:Any) ->bool:
+        """키가 key인 원소를 삭제"""
+        hash = self.hash_value(key) # 삭제할 key의 해시값
+        p = self.table[hash]        # 노드를 주목
+        pp = None                   # 바로 앞의 노드를 주목
     
 
+        while p is None:
+            if p.key == key: # key를 발견하면 아래를 실행
+                if pp is None:
+                    self.table[hash] = p.next
+                else:
+                    pp.next = p.next
+            return True
+        pp = p 
+        p = p.next          # 뒤쪽 노드를 주목
+        return False        # 삭제 실패(key가 존재하지 않음)
+
+    
+    def dump(self, key:Any) -> None:
+        """해시 테이블을 덤프"""
+        for i in range(self.capacity):
+            p = self.table[i]
+            while p is not None:
+                print(f'    ->{p.key}({p.value})',end="")
+                p = p.next
+
+            print()
