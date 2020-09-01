8451fb784e90047790a2b00e36cc7a38c3ad31ad
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-09-01 16:11:11 +0900

    Add files via upload

--- recursion_algorithm/recur.py
+++ recursion_algorithm/recur.py
@@ -9,3 +9,15 @@ def recur(n:int)->int:
 x = int(input('정숫값을 입력하세요 : '))
 
 recur(x)
+
+# 꼬리 재귀 제거하기
+
+def recur_remove_tail(n:int) ->int :
+    while n > 0 :
+        recur_remove_tail(n-1)
+        print(n)
+        n = n - 2
+
+y = int(input('정숫값을 입력하세요 : '))
+
+recur_remove_tail(y)
