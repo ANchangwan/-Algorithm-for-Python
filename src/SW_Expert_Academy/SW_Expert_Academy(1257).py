ee3f21c1e49eeacbceb797661dd195a423cdbf6c
ANchangwan <56478474+ANchangwan@users.noreply.github.com>
2020-11-12 15:35:24 +0900

    Add files via upload

---
+++ src/SW_Expert_Academy/SW_Expert_Academy(1257).py
@@ -0,0 +1,19 @@
+# 1501023 안창완
+# 1257 알고리즘 문제
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    K = int(input())
+    ipt = input()
+    sorted_lst = []
+
+    for i in range(len(ipt)):
+        for j in range(i,len(ipt)):
+            sorted_lst.append(ipt[i:j+1])
+    sorted_lst2 = sorted(list(set(sorted_lst)))
+    if K > len(sorted_lst2):
+        lst = 'None'
+    else:
+        lst = sorted_lst2[K-1]
+    print("#{} {}".format(test_case,lst))
