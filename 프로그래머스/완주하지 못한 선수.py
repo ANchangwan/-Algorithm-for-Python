6b243595aee84a1512c5097f9d1862244a54848b
Anchangwan <56478474+ANchangwan@users.noreply.github.com>
2021-05-11 20:27:13 +0900

    Add files via upload

--- 프로그래머스/완주하지 못한 선수.py
+++ 프로그래머스/완주하지 못한 선수.py
@@ -1,3 +1,4 @@
+# 1
 def solution(participant,completion):
     answer = ""
     participant.sort()
@@ -9,3 +10,26 @@ def solution(participant,completion):
     if answer == "":
         answer = participant[len(completion)]
     return answer
+
+# 2
+def solution(participant, completion):
+    answer = ''
+    participant.sort()
+    completion.sort()
+    for x,y in zip(participant,completion):
+        if x != y:
+            answer = x
+            return answer
+    return participant[-1]
+
+
+
+# Collections 라이브러리 활용
+import collections
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
+
+
+
